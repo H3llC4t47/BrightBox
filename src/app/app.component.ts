633c49e4610c5db3c9c45dd02bd3daea33dbe034
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-05-20 15:46:40 +0200

    konečně

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,22 @@
-import {Component} from '@angular/core';
-import {Params} from './params';
+import {Component, OnInit} from '@angular/core';
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {observable, Observable, of} from 'rxjs';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {CommunicationService} from './communication.service';
-import * as backup from '../backup.json';
+import * as backup from '../assets/backup.json';
 
-interface JSON {
+interface Data {
+    items: Item[];
+}
+
+interface Item {
     isMonthly: boolean;
     isGrouped: boolean;
     catID: number;
     name: string;
     price: number;
-    label: string;
 }
 
 @Component({
@@ -23,22 +24,12 @@ interface JSON {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     title = 'BrightBoxFrontEnd';
-
-    /**
-     *    constructor(private apiService: HttpService) {
-     *    }
-     *
-     *    VytvoritUzivatele(): void {
-     *        this.apiService.HttpGetRUpdate().subscribe((info) => {
-     *            this.Prijmuto = info;
-     *        });
-     *    }
-     */
+    temps;
     global = true;
     global2 = false;
-    jsonveci = backup;
+    jsonveci: Data
     doc = document;
     clientname = '';
     firmname = '';
@@ -57,6 +48,21 @@ export class AppComponent {
     reprezentant = '';
     emailreprezentant = '';
     phonereprezentant = '';
+
+    ngOnInit(): void {
+        this.HttpGetRUpdate();
+        console.log(this.jsonveci);
+    }
+
+    constructor(private httpClient: HttpClient) {
+    }
+
+    public HttpGetRUpdate() {
+        this.httpClient.get<Data>('http://127.0.0.1:8080/fuj-backend/api/polozky/data', {}).subscribe(
+            val => this.jsonveci = val,
+            () => this.httpClient.get<Data>('/assets/backup.json').subscribe(val => this.jsonveci = val)
+        );
+    }
 
     hide() {
         this.global = !this.global;
