4d611417461dc039360f1ce441d0d564c7405228
skyper-cz <62872636+skyper-cz@users.noreply.github.com>
2021-05-21 14:31:23 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -77,38 +77,42 @@ export class AppComponent implements OnInit {
     }
 
     PocitaniCastky() {
-        for (this.i = 0; this.i < this.jsonveci.items.length; this.i++) {
-            for (this.i = 0; this.i < this.jsonveci.items.length; this.i++) {
-                if (this.isselected[this.i][this.x] === true) {
-                    if (this.jsonveci.items[this.i][this.x].isGrouped === true) {
-                        this.groupcount++;
-                    } else {
-                        this.totalprice += this.jsonveci.items[this.i][this.x].price;
+        while ((typeof this.jsonveci.items[this.i] !== 'undefined')) {
+            while ((typeof this.jsonveci.items[this.x] !== 'undefined')) {
+                {
+                    if (this.isselected[this.i][this.x] === true) {
+                        if (this.jsonveci.items[this.i][this.x].isGrouped === true) {
+                            this.groupcount++;
+                        } else {
+                            this.totalprice += this.jsonveci.items[this.i][this.x].price;
+                        }
                     }
+                    this.x++;
                 }
-            }
-            switch (this.groupcount) {
-                case 1:
-                    this.groupamount = 6500;
-                    break;
-                case 2:
-                    this.groupamount = 12000;
-                    break;
-                case 3:
-                    this.groupamount = 16500;
-                    break;
-                case 4:
-                    this.groupamount = 20000;
-                    break;
-                default:
-                    this.groupamount = 0;
-                    break;
+                this.i++;
+                switch (this.groupcount) {
+                    case 1:
+                        this.groupamount = 6500;
+                        break;
+                    case 2:
+                        this.groupamount = 12000;
+                        break;
+                    case 3:
+                        this.groupamount = 16500;
+                        break;
+                    case 4:
+                        this.groupamount = 20000;
+                        break;
+                    default:
+                        this.groupamount = 0;
+                        break;
+
+                }
+                this.totalprice += this.groupamount;
+                this.groupamount = 0;
+                this.groupcount = 0;
 
             }
-            this.totalprice += this.groupamount;
-            this.groupamount = 0;
-            this.groupcount = 0;
-
         }
     }
 }
